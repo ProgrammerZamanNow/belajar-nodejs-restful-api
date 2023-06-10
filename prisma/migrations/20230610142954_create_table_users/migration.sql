d9f8966303f63793c73f17d97efbe58e62c2c87f
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-06-10 21:31:55 +0700

    user model

---
+++ prisma/migrations/20230610142954_create_table_users/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `username` VARCHAR(100) NOT NULL,
+    `password` VARCHAR(100) NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `token` VARCHAR(100) NULL,
+
+    PRIMARY KEY (`username`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE InnoDB;
