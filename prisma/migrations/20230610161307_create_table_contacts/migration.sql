70b6f9be1e053c5858b72aa2ad2ebe2ef47b302e
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-06-10 23:15:07 +0700

    contact model

---
+++ prisma/migrations/20230610161307_create_table_contacts/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `contacts` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(100) NOT NULL,
+    `last_name` VARCHAR(100) NULL,
+    `email` VARCHAR(200) NULL,
+    `phone` VARCHAR(20) NULL,
+    `username` VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE InnoDB;
+
+-- AddForeignKey
+ALTER TABLE `contacts` ADD CONSTRAINT `contacts_username_fkey` FOREIGN KEY (`username`) REFERENCES `users`(`username`) ON DELETE RESTRICT ON UPDATE CASCADE;
