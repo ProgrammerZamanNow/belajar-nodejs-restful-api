36fd1e0eef0709bd2ab322954301f6adc2736d11
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-06-10 23:19:54 +0700

    address model

---
+++ prisma/migrations/20230610161759_create_addresss_table/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE `addresses` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `street` VARCHAR(255) NULL,
+    `city` VARCHAR(100) NULL,
+    `province` VARCHAR(100) NULL,
+    `country` VARCHAR(100) NOT NULL,
+    `postal_code` VARCHAR(10) NOT NULL,
+    `contact_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE InnoDB;
+
+-- AddForeignKey
+ALTER TABLE `addresses` ADD CONSTRAINT `addresses_contact_id_fkey` FOREIGN KEY (`contact_id`) REFERENCES `contacts`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
